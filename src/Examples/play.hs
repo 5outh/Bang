fbd5ee792dfb8ed3690d4be960ceeb8a2955b5d4
Benjamin Kovach <bkovach13@gmail.com>
2014-05-03 23:09:22 -0400

    Add complex example to Examples

--- src/Examples/play.hs
+++ src/Examples/play.hs
@@ -1,3 +1,12 @@
 import Bang
 
-main = playIO $ (120 <>> hc) <&> (120 <>> (bd >> r >> sn >> r))
+simple = playIO $ (120 <>> hc) <&> (120 <>> (bd >> r >> sn >> r))
+
+complex = playIO $ 240 <>> do
+  bass & cc
+  half $ do
+    sn
+    quad $ 4 $> hc & bd
+    sn & ho
+    (bass & ch)
+  mirror $ double $ 3 $> ((sn & hc) >> bd >> (sn & ho))
