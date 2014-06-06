b5c2131a94ed719393368b3182b41614ae398cd3
Benjamin Kovach <bkovach13@gmail.com>
2014-06-06 09:00:34 -0400

    Add take and drop, hushFor and hushFrom

--- src/Bang/Music/Transform.hs
+++ src/Bang/Music/Transform.hs
@@ -8,7 +8,7 @@ reverseMusic :: Music Dur b -> Music Dur b
 reverseMusic p@(Prim _) = p
 reverseMusic (a :+: b) = reverseMusic b :+: reverseMusic a
 reverseMusic (a :=: b) =
-  if durA < durB then (rest diff :+: reverseMusic a) :=: reverseMusic b
+  if      durA < durB then (rest diff :+: reverseMusic a) :=: reverseMusic b
   else if durB < durA then reverseMusic a :=: (rest diff :+: reverseMusic b)
   else reverseMusic a :=: reverseMusic b
   where (durA, durB) = (duration a, duration b)
@@ -21,7 +21,7 @@ mirror m = m <> reverseMusic m
 cross :: Music Dur b -> Music Dur b
 cross m = m >< reverseMusic m
 
-takeDur :: Dur -> Music Dur a -> Music Dur a
+takeDur :: Dur -> Music Dur b -> Music Dur b
 takeDur d = go d
   where go dr m | dr <= 0 = rest (abs dr)
                 | otherwise = case m of
@@ -30,3 +30,18 @@ takeDur d = go d
                     (a :=: b)    -> (go dr a) :=: (go dr b)
                     (Modify c a) -> Modify c (go dr a)
 
+dropDur :: Dur -> Music Dur b -> Music Dur b
+dropDur d = go d
+  where go dr m | dr <= 0 = m
+                | otherwise = case m of
+                    p@(Prim (Rest d'))   -> rest 0
+                    p@(Prim (Note d' _)) -> rest 0
+                    (a :+: b)    -> (go dr a) :+: (go (dr - duration a) b)
+                    (a :=: b)    -> (go dr a) :=: (go dr b)
+                    (Modify c a) -> Modify c (go dr a)
+
+hushFor :: Dur -> Music Dur b -> Music Dur b
+hushFor d m = rest d <> dropDur d m
+
+hushFrom :: Dur -> Music Dur b -> Music Dur b
+hushFrom d m = takeDur d m <> rest (max (duration m - d) 0)
