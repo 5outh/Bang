5e389edcff9edb573ef8590e343b63d217cf00f3
Benjamin Kovach <bkovach5@uga.edu>
2015-05-31 14:12:07 -0400

    Change and type annotate all examples to pure data instead of IO ops

--- src/Examples/play.hs
+++ src/Examples/play.hs
@@ -3,11 +3,13 @@ import Data.Monoid
 import Data.Ratio
 
 -- | The very first rhythm most drummers learn to play.
-simple = bangR $ double $ (4 #> hc) >< (bd <> qr <> sn <> qr)
+simple :: Music Dur PercussionSound
+simple = double $ (4 #> hc) >< (bd <> qr <> sn <> qr)
 
 -- | An example of taking simple pieces and doing crazy stuff with it.
-complex = bang $
-  mirror $ double $ 
+complex :: Music Dur PercussionSound
+complex = mirror $
+  double $
   mconcat [
     bd >< cc
   , cross $ half $ mconcat [
@@ -21,60 +23,68 @@ complex = bang $
   ]
 
 -- A double-bass (sort of) beat in triplets
-doubleBass = bangR $ 6 !> triplets ( hc >< (3 #> bassDrum2) )
+doubleBass :: Music Dur PercussionSound
+doubleBass = 6 !> triplets ( hc >< (3 #> bassDrum2) )
 
 -- | A simple 3/4 polyrhythm, using the ~=~ operator.
+polyrhythm :: Music Dur PercussionSound
 polyrhythm = (3, 3 #> bd) ~=~ (4, 4 #> sn)
 
 -- | Some quintuplets!
+quints :: Music Dur PercussionSound
 quints = tempo 4 $ quintuplets $ (hc >< bd) <> (4 #> bd)
 
 -- | Intro to 'Toxicity' by System of a Down.
+toxicityIntro :: Music Dur PercussionSound
 toxicityIntro =
   let sh = sn >< hc -- snare and closed hi-hat combo
       bc = bd >< hc -- bass and closed hi-hat combo
       cd = bd >< cc -- bass and crash cymbal combo
-  in bang $ 
-     double $ 
+  in double $
      bd
-     <> ( double $ 
+     <> ( double $
           mconcat [
             mconcat [sh, bd, qr, bd, sh, qr, bd, qr, sh, qr]
-          , mconcat [ 
+          , mconcat [
               (2 #>) >>~ [sn, t1, t2] -- play each element of the list twice
             , double $ 4 #> sn
-            , (2 #>) >>~ [sn, t1, t2] 
-            , m4 cd qr hc sn -- groups of measures with 4 beats  
+            , (2 #>) >>~ [sn, t1, t2]
+            , m4 cd qr hc sn -- groups of measures with 4 beats
             , m4 hc bd sh qr
             , m4 hc sn bc qr
             , m4 bc qr hc sh
             , m4 hc bd sh qr
-            , m4 bd qr sh qr 
-            ] 
+            , m4 bd qr sh qr
+            ]
           ] )
 
 -- | Plays for 2 measures, with original duration of 6 measures.
-changeDurationEx = bangR $ 2 ~~ mconcat [
+changeDurationEx :: Music Dur PercussionSound
+changeDurationEx = 2 ~~ mconcat [
     16 #> bd
   , 4 #> sn
   , wr
   ]
 
 -- | A 3/4 polyrhythm fitted to the duration of the shorter (3-beat) section
-polyFast = bang $ (3 #> bd) ~= (4 #> sn)
+polyFast :: Music Dur PercussionSound
+polyFast = (3 #> bd) ~= (4 #> sn)
 
 -- | A 3/4 polyrhythm fitted to the duration of the longer (4-beat) section
-polySlow = bang $ (3 #> bd) =~ (4 #> sn)
+polySlow :: Music Dur PercussionSound
+polySlow = (3 #> bd) =~ (4 #> sn)
 
--- | Play each element of the list sequentially with duration 1 
-normalizeEx = bangR $ 1 <!> [
+-- | Play each element of the list sequentially with duration 1
+normalizeEx :: Music Dur PercussionSound
+normalizeEx = 1 <!> [
     12 #> bd
   , 4 #> hc
   , 3 #> sn
   ]
 
 -- Play each element of the list concurrently with duration 1/2
-normalizeCEx = bangR $ (1/2) >!< [
+normalizeCEx :: Music Dur PercussionSound
+normalizeCEx = (1/2) >!< [
     12 #> bd
   , 4  #> hc
   , 3  #> sn
